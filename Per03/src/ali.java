c43b9db6e5388e9af19c4bd9221013017450d59f
unknown <Ali@.(none)>
2014-07-05 15:35:20 +0700

    Tugas P1

---
+++ Per03/src/ali.java
@@ -0,0 +1,7 @@
+package ali.bin;
+
+public class ali{
+	public static void main(String[] ali){
+		System.out.println("Jurusan Teknik Informatika");
+	}
+}
---
+++ Per03/src/biodata.java
@@ -0,0 +1,9 @@
+package ali.bin;
+
+public class biodata{
+	public static void main(String[] ali){
+		System.out.println("Nama   : Mukhamat Ali");
+		System.out.println("Alamat : Sunter");
+		System.out.println("Status : Lajang");
+	}
+} 
